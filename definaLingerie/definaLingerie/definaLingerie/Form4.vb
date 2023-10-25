6d9686ef441fb3a4df7a28a6ba2a117b4f071ac2
Samuel Villar <villar.mucasan@gmail.com>
2023-10-24 21:22:29 -0300

    Subindo atualização das telas de cadastro, edição e deleção de funcionários

--- definaLingerie/definaLingerie/definaLingerie/Form4.vb
+++ definaLingerie/definaLingerie/definaLingerie/Form4.vb
@@ -1,29 +1,39 @@
-﻿Public Class Form4
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+﻿Imports System.Data.SQLite
 
+Public Class Form4
+    Private dbCommand As String = ""
+    Private bindingSrc As BindingSource
+
+    Private dbName As String = "banco.db"
+    Private dbPath As String = Application.StartupPath & "\banco\" & dbName
+    Private conString As String = "Data Source=" & dbPath & ";Version=3"
+
+    Private connection As New SQLiteConnection(conString)
+
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connection.Open()
     End Sub
 
-    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+    Private Sub btn_criar_Click(sender As Object, e As EventArgs) Handles btn_criar.Click
+        Dim sql As String = "SELECT * FROM tb_funcionarios WHERE usuario='" & txt_user.Text & "';"
 
-    End Sub
 
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim command As New SQLiteCommand(sql, connection)
+        Using reader As SQLiteDataReader = command.ExecuteReader()
+            If reader.Read() Then
+                If reader("usuario") = txt_user.Text Or reader("nome") = txt_nome.Text Then
+                    MsgBox("Há um registro com esse usuário ou nome, insira outro nome ou usuário!", MsgBoxStyle.OkOnly + MsgBoxStyle.Critical)
+                    txt_user.Clear()
+                    txt_nome.Clear()
+                End If
+            Else
 
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
-
-    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles cmb_data.ValueChanged
-
+                MsgBox("CRIAÇÃO EFETUADA COM SUCESSO!")
+                sql = "INSERT INTO tb_funcionarios VALUES ('" & txt_cpf.Text & "','" & txt_nome.Text & "','" & txt_user.Text & "','" & txt_senha.Text & "','" & cmb_data.Text & "','" & txt_email.Text & "','" & txt_telefone.Text & "',2);"
+                Dim command2 As New SQLiteCommand(sql, connection)
+                command2.ExecuteNonQuery()
+            End If
+        End Using
+        connection.Close()
     End Sub
 End Class
